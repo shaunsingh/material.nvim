cea2fd7379535664b220bf98ba2a88eeb7e9aa01
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-01 22:39:10 -0400

    Update colors.lua

--- lua/material/colors.lua
+++ lua/material/colors.lua
@@ -24,58 +24,9 @@ local material = {
 	none =			'NONE'
 }
 
-
 -- Style specific colors
 
-if vim.g.material_style == 'darker' then
-	-- Darker theme style
-
-	material.bg =			'#011627'
-	material.bg_alt =		'#1A1A1A'
-	material.fg =			'#B0BEC5'
-	material.text =			'#727272'
-	material.comments =		'#616161'
-	material.selection = 	'#404040'
-	material.contrast =		'#1A1A1A'
-	material.active =		'#323232'
-	material.border =		'#292929'
-	material.line_numbers =	'#424242'
-	material.highlight =	'#3F3F3F'
-	material.disabled =		'#474747'
-	material.accent =		'#FF9800'
-
-elseif vim.g.material_style == 'lighter' then
-	-- Lighter theme style
-
-	material.bg =			'#FAFAFA'
-	material.bg_alt =		'#FFFFFF'
-	material.fg =			'#546E7A'
-	material.text =			'#94A7B0'
-	material.comments =		'#AABFC9'
-	material.selection = 	'#80CBC4'
-	material.contrast =		'#EEEEEE'
-	material.active =		'#E7E7E8'
-	material.border =		'#D3E1E8'
-	material.line_numbers =	'#CFD8DC'
-	material.highlight =	'#E7E7E8'
-	material.disabled =		'#D2D4D5'
-	material.cursor =		'#272727'
-	material.accent =		'#00BCD4'
-
-	material.white =		'#FFFFFF'
-	material.gray = 		'#717CB4'
-	material.black =		'#000000'
-	material.red =  		'#E53935'
-	material.green =		'#91B859'
-	material.yellow =		'#F6A434'
-	material.blue = 		'#6182B8'
-	material.paleblue =		'#8796B0'
-	material.cyan = 		'#39ADB5'
-	material.purple =		'#7C4DFF'
-	material.orange =		'#F76D47'
-	material.pink = 		'#FF5370'
-
-elseif vim.g.material_style == 'palenight' then
+if vim.g.material_style == 'moonlight' then
 	-- Palenight theme style
 
 	material.bg =			'#2f334d'
@@ -91,41 +42,6 @@ elseif vim.g.material_style == 'palenight' then
 	material.highlight =	'#444267'
 	material.disabled =		'#515772'
 	material.accent =		'#AB47BC'
-
-elseif vim.g.material_style == 'deep ocean' then
-	-- Deep Ocean theme style
-
-	material.bg =			'#0F111A'
-	material.bg_alt =		'#090B10'
-	material.fg =			'#8F93A2'
-	material.text =			'#717CB4'
-	material.comments =		'#464B5D'
-	material.selection = 	'#1F2233'
-	material.contrast =		'#090B10'
-	material.active =		'#1A1C25'
-	material.border =		'#1f2233'
-	material.line_numbers =	'#3B3F51'
-	material.highlight =	'#1F2233'
-	material.disabled =		'#464B5D'
-	material.accent =		'#84FFFF'
-
-else vim.g.material_style = 'oceanic'
-	-- Oceanic theme style
-
-	material.bg =			'#263238'
-	material.bg_alt =		'#192227'
-	material.fg =			'#B0BEC5'
-	material.text =			'#607D8B'
-	material.comments =		'#464B5D'
-	material.selection = 	'#546E7A'
-	material.contrast =		'#1E272C'
-	material.active =		'#314549'
-	material.border =		'#2A373E'
-	material.line_numbers =	'#37474F'
-	material.highlight =	'#425B67'
-	material.disabled =		'#415967'
-	material.accent =		'#009688'
-
 end
 
 -- Optional colors
