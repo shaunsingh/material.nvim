2352a9ed45edac7f2e64cdf34da7476fd32e97f3
shaunsingh <71196912+shaunsingh@users.noreply.github.com>
2021-05-10 16:14:05 -0400

    Update theme.lua

--- lua/material/theme.lua
+++ lua/material/theme.lua
@@ -155,7 +155,7 @@ theme.loadEditor = function ()
 
         -- Dashboard
         DashboardShortCut =                     { fg = material.blue },
-        DashboardHeader =                       { fg = material.green },
+        DashboardHeader =                       { fg = material.red },
         DashboardCenter =                       { fg = material.cyan },
         DashboardFooter =                       { fg = material.green, style = "italic" },
 
